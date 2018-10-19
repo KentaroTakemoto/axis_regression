3b0c3e7fd3b3d2f5edae31702064c5f5b7c31edb
KentaroTakemoto <takemoto@MacBook-Pro-6.local>
2018-10-20 08:21:42 +0900

    main files

--- test.py
+++ test.py
@@ -12,6 +12,7 @@ parser.add_argument('--gpu', '-g', default=-1, type=int,
                     help='GPU ID (negative value indicates CPU)')
 parser.add_argument('--test_angles', '-te', default='60,165', type=str)
 parser.add_argument('--train_view_params', '-tr', default=90, type=int)
+parser.add_argument('--view_params_file', '-vi', default='/home/mil/takemoto/other_githubs/RenderForCNN/test_results/view_params.txt', type=str)
 parser.add_argument('--weight', '-w', default="weight/test1.weight", type=str)
 parser.add_argument('--image_size', default=224, type=int)
 parser.add_argument('--output', default=7, type=int)
@@ -19,6 +20,11 @@ args = parser.parse_args()
 
 train_view_params = args.train_view_params
 test_angles = args.test_angles.split(',')
+view_params_file = args.view_params_file
+
+with open(view_params_file,"r") as f:
+    ls = f.readlines()
+view_params = [l.rstrip('\n') for l in ls]
 
 model = VGG_double(n_out=args.output)
 serializers.load_npz(args.weight, model)
