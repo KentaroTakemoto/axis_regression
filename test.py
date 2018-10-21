7f50bef1aa91a567ae12f2192523e58bb40742b8
KentaroTakemoto <takemoto@MacBook-Pro-6.local>
2018-10-21 21:52:42 +0900

    main files

--- test.py
+++ test.py
@@ -58,9 +58,9 @@ for i in range(100):
 print(preds[0])
 print('-'*40)
 print(labels[0])
-preds = chainer.cuda.to_cpu(np.array(preds))
+preds = chainer.cuda.to_cpu(np.array(preds)).astype(np.float32)
 
-labels = chainer.cuda.to_cpu(np.array(labels))
+labels = chainer.cuda.to_cpu(np.array(labels)).astype(np.float32)
 
 if args.output == 7:
     print('l2 error for direction : {}'.format(np.mean(np.linalg.norm(preds[:,:3]-labels[:,:3],axis=1))))
