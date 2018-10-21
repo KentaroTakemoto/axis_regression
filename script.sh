1ff5c0ed32db986d81ab11937ab7febc01332122
KentaroTakemoto <takemoto@MacBook-Pro-6.local>
2018-10-21 21:47:35 +0900

    main files

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@
 # python train.py --gpu 1 -b 20 -e 100 --output 3 -s test2
 # python train.py --gpu 1 -b 20 -e 100 --output 4 -s test3
 
-python test.py --gpu 2 -w weight/test1.weight --output 7
+python test.py --gpu 1 -w weight/test1.weight --output 7
