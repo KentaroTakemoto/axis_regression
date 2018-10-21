03edff118e75d769794dc968eb86227f716ff5c9
KentaroTakemoto <takemoto@MacBook-Pro-6.local>
2018-10-21 22:24:10 +0900

    main files

---
+++ model_softmax.py
@@ -0,0 +1,34 @@
+import math
+
+import numpy as np
+import chainer
+import chainer.links as L
+import chainer.functions as F
+from chainer import serializers
+from chainer import Variable
+
+class VGG_double_softmax(chainer.Chain):
+    def __init__(self, n_out=7):
+        super(VGG_double, self).__init__(
+            model = L.VGG16Layers(),
+            fc = L.Linear(8192,n_out))
+
+    def __call__(self, x, t=None, train=True, finetune=False):
+        with chainer.using_config('enable_backprop', finetune):
+            x1 = Variable(self.xp.asarray(x.data[:,:3,:,:],dtype=np.float32))
+            x2 = Variable(self.xp.asarray(x.data[:,3:,:,:],dtype=np.float32))
+
+        with chainer.using_config('train', finetune):
+            h1 = self.model(x1,layers=['fc7'])['fc7']
+            h2 = self.model(x2,layers=['fc7'])['fc7']
+        h = F.concat([h1, h2], axis=1)
+        if finetune==False:
+            with chainer.using_config('enable_backprop', train):
+                h = Variable(h.data)
+        h = F.tanh(self.fc(h))
+        sum = self.xp.sum(h**2,axis=1)
+        sum_t = self.xp.ones_like(sum)
+        if train:
+            return F.mean_squared_error(h, t) + F.mean_squared_error(sum, sum_t)
+        else:
+            return h
--- script.sh
+++ script.sh
@@ -4,6 +4,8 @@
 # python train.py --gpu 1 -b 20 -e 100 --output 3 -s test2
 # python train.py --gpu 1 -b 20 -e 100 --output 4 -s test3
 
-python test.py --gpu 1 -w weight/test1.weight --output 7
-python test.py --gpu 1 -w weight/test2.weight --output 3
-python test.py --gpu 1 -w weight/test3.weight --output 4
+python train_softmax.py --gpu 1 -b 20 -e 100 --output 3 -s test4
+
+# python test.py --gpu 1 -w weight/test1.weight --output 7
+# python test.py --gpu 1 -w weight/test2.weight --output 3
+# python test.py --gpu 1 -w weight/test3.weight --output 4
