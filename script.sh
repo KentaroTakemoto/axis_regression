c664a24e9f8202f75578dbea400aab891c693e52
KentaroTakemoto <takemoto@MacBook-Pro-6.local>
2018-10-22 12:02:00 +0900

    main files

--- script.sh
+++ script.sh
@@ -6,13 +6,13 @@
 
 # python train_softmax.py --gpu 1 -b 20 -e 100 --output 3 -s test4
 # python train_conv.py --gpu 0 -b 20 -e 20 --output 7 -s test5
-python train_conv.py --gpu 0 -b 20 -e 20 --output 3 -s test6
+# python train_conv.py --gpu 0 -b 20 -e 20 --output 3 -s test6
 # python train_conv.py --gpu 0 -b 20 -e 20 --output 4 -s test7
 
-# python test.py --gpu 0 -w weight/test1.weight --output 7
-# python test.py --gpu 0 -w weight/test2.weight --output 3
-# python test.py --gpu 0 -w weight/test3.weight --output 4
-# python test_softmax.py --gpu 0 -w weight/test4.weight --output 3
-# python test_conv.py --gpu 0 -w weight/test5.weight --output 7
-# python test_conv.py --gpu 0 -w weight/test6.weight --output 3
-# python test_conv.py --gpu 0 -w weight/test7.weight --output 4
+python test.py --gpu 0 -w weight/test1.weight --output 7
+python test.py --gpu 0 -w weight/test2.weight --output 3
+python test.py --gpu 0 -w weight/test3.weight --output 4
+python test_softmax.py --gpu 0 -w weight/test4.weight --output 3
+python test_conv.py --gpu 0 -w weight/test5.weight --output 7
+python test_conv.py --gpu 0 -w weight/test6.weight --output 3
+python test_conv.py --gpu 0 -w weight/test7.weight --output 4
